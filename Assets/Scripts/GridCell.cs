b2d868e60bb8b075cd4da88ffe952af82d8fc3dd
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-10-28 00:30:50 -0500

    - Refactored GridCell class.

--- Assets/Scripts/GridCell.cs
+++ Assets/Scripts/GridCell.cs
@@ -6,97 +6,20 @@ using UnityEngine;
 
 public class GridCell : MonoBehaviour
 {
-    public Action<GridCell> OnCellAlive;
-    public Action<GridCell> OnCellDead;
-
     [SerializeField] private SpriteRenderer _renderer;
-    [SerializeField] private Color _aliveColor;
-    [SerializeField] private Color _deadColor;
     [SerializeField] private bool _isAlive = false;
 
-    //public int LiveNeighbourCount = 0;
-
-    // Check to make sure cell is only updated once when grid updates. This is required because cells update neighbouring cells.
-    public bool CellUpdated = false;
     public bool IsAlive => _isAlive;
-    public bool PreviouslyAlive { get; private set; } = false;
     public Vector2 SpriteBounds => _renderer.sprite.bounds.size;
 
-    //private bool _previouslyAlive;
     private List<GridCell> _neighbours = new List<GridCell>();
 
-    private bool _nextState = false;        // true = alive, false = dead
-
-    // Used by player inputs.
-    /*public void ManualToggleCellState()
-    {
-        ToggleCellState(!IsAlive);
-    }*/
-
-    // Used automatically. Only runs if: is alive and updated by the grid, or if is dead and updated by a neighbouring alive cell.
-    /*public void UpdateCellStateOld()
-    {
-        //if (IsAlive && CellUpdated == true) return;
-
-        // Update self.
-        int liveNeighbours = _neighbours.Count((neighbour) => neighbour.PreviouslyAlive);
-        //LiveNeighbourCount = liveNeighbours;
-        
-        //print(liveNeighbours);
-        if (!IsAlive)
-        {
-            //Debug.LogError("OMG ITS ALIVE ALIVE NEIGHBOUR ALIVE!");
-            if (liveNeighbours == 3) ToggleCellState(true);
-        }
-        else
-        {
-            if (_previouslyAlive)
-            {
-                //print("ENABLING DEAD NEIGHBOURS");
-                List<GridCell> deadNeighbours = new();// = _neighbours.FindAll((cell) => cell.IsAlive == false);
-                foreach (GridCell cell in _neighbours) { if (cell.IsAlive == false) deadNeighbours.Add(cell); }
-                foreach (GridCell cell in deadNeighbours)
-                {
-                    //Debug.LogWarning("TURNING ON DEAD NEIGHBOURS!!!");
-                    cell.UpdateCellState();
-                }
-            }
-            if (liveNeighbours < 2 || liveNeighbours > 3) ToggleCellState(true);
-            else ToggleCellState(false);
-            // Update dead neighbours.
-        }
-        //_previouslyAlive = _isAlive;
-        //if (_previouslyAlive != false && _isAlive != false) CellUpdated = true;
-    }*/
-
-    /*public void UpdateCellState()
-    {
-        // Update self.
-        //PreviouslyAlive = _isAlive;
-        //int liveNeighbours = _neighbours.Count((neighbour) => neighbour.PreviouslyAlive);
-        int liveNeighbours = 0;
-        foreach (GridCell cell in _neighbours) if (cell.PreviouslyAlive == true) liveNeighbours++;
-        //LiveNeighbourCount = liveNeighbours;
-        if (!_isAlive == false && liveNeighbours == 3)
-        {
-            ToggleCellState(true);      // Reproduction.
-        }
-        else
-        {
-            if (liveNeighbours >= 2 && liveNeighbours <= 3) ToggleCellState(true);      // Keep alive.
-            else ToggleCellState(false);        // Overpopulation / underpopulation.
-        }
-    }*/
+    private bool _nextState = false;        // True = Alive, False = Dead.
 
     public void UpdateCellState()
     {
         // Update self.
-        //PreviouslyAlive = _isAlive;
-        //int liveNeighbours = _neighbours.Count((neighbour) => neighbour.PreviouslyAlive);
-        //int liveNeighbours = 0;
-        //foreach (GridCell cell in _neighbours) if (cell.IsAlive == true) liveNeighbours++;
         int liveNeighbours = _neighbours.Count((neighbour) => neighbour.IsAlive);
-        //LiveNeighbourCount = liveNeighbours;
         if (_isAlive == false)
         {
             if (liveNeighbours == 3)
@@ -107,29 +30,20 @@ public class GridCell : MonoBehaviour
         }
         else
         {
-            if (liveNeighbours >= 2 && liveNeighbours <= 3) _nextState = true;      // Keep alive.
-            else _nextState = false;        // Overpopulation / underpopulation.
+            if (liveNeighbours < 2 || liveNeighbours > 3) _nextState = false;      // Overpopulation / underpopulation.
+            else _nextState = true;        // Stay alive.
         }
     }
 
     public void ToggleCellState(bool newAliveState)
     {
         _isAlive = newAliveState;
-        //_nextState = _isAlive;
         UpdateColor();
-
-        //if (isAlive) OnCellAlive.Invoke(this);
-        //else OnCellDead.Invoke(this);
     }
 
     public void AddNeighbour(GridCell neighbour)
     {
         _neighbours.Add(neighbour);
-    }
-
-    public void SaveCurrentState()
-    {
-        PreviouslyAlive = _isAlive;
     }
 
     public void ApplyNextState()
